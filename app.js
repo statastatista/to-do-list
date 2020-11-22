47bfc4ff0953b069ae67cd17d4942b555bf36265
statastatista <74862625+statastatista@users.noreply.github.com>
2020-11-22 17:59:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -82,11 +82,3 @@ function filterTodo(e) {
   });
 }
 
-function saveLocalTodos(todo){
-  let todos;
-  if (localStorage.getItem('todos') === null){
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem('todos'));
-  }
-}
